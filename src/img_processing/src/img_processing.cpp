b2180511e5356c2b39dc7f3d8d4f4ecc5820ebbf
pisit <pisitindex@indexlivingmall.com>
2022-12-09 17:16:55 +0700

    register warp perspective

--- src/img_processing/src/img_processing.cpp
+++ src/img_processing/src/img_processing.cpp
@@ -1,5 +1,6 @@
 #include <img_processing/img_processing.hpp>
 #include <cira_lib_bernoulli/CiraBernoulliWidget.h>
+#include <models/warp_perspective_ai/warp_perspective_aiModel.hpp>
 #include <models/camera_calibration/camera_calibrationModel.hpp>
 #include <models/undistort/undistortModel.hpp>
 
@@ -11,7 +12,7 @@ std::shared_ptr<DataModelRegistry> img_processing::registerDataModels(std::share
 
   ret->registerModel<camera_calibrationModel>();
   ret->registerModel<undistortModel>();
-
+  ret->registerModel<warp_perspective_aiModel>();
 
   setTreeWidget();
   return ret;
@@ -26,5 +27,6 @@ void img_processing::setTreeWidget() {
   category = "img_processing";
   strListNodeDataModels << category + ",camera_calibration"+ ",null";
   strListNodeDataModels << category + ",undistort"+ ",null";
+  strListNodeDataModels << category + ",warp_perspective_ai"+ ",null";
   CiraBernoulliWidget::pluginTreeWidget->addNodeDataModels(strListNodeDataModels);
 }
